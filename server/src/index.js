2052d00721e685096b73739bf327fb76cd0115cb
aimalshah <aimalshah62@gmail.com>
2024-04-19 08:21:12 +0500

    cors fixed

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ import { authRouter } from "./routes/auth.js";
 import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended : false}))
